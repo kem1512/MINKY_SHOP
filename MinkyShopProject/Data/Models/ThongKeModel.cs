a8b385cc70c13a39a740937ba246b796b87a73eb
gnt.hiepnt <hiepnt@geneat.vn>
2023-03-21 14:14:45 +0700

    ThongkeSpBanNhieuNhat

---
+++ MinkyShopProject/Data/Models/ThongKeModel.cs
@@ -0,0 +1,9 @@
+﻿namespace MinkyShopProject.Data.Models
+{
+    public class SoLuongThangNam
+    {
+        public int nam { get; set; }
+        public int thang { get; set; }
+        public int soluong { get; set; }
+    }
+}
