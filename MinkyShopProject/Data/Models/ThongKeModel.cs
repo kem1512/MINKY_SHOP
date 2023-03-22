619d0496f290700e4a1c20b4d434fceff69ac1b3
gnt.hiepnt <hiepnt@geneat.vn>
2023-03-22 11:36:53 +0700

    thong ke +2, fix anh khachhang

--- MinkyShopProject/Data/Models/ThongKeModel.cs
+++ MinkyShopProject/Data/Models/ThongKeModel.cs
@@ -6,4 +6,21 @@
         public int thang { get; set; }
         public int soluong { get; set; }
     }
+    public class TongTienNgayTienThangNam
+    {
+        public float TongTien { get; set; }
+        public int Ngay { get; set; }
+        public int thang { get; set; }
+        public int nam { get; set; }
+
+    }
+    public class NhanVienBanHangNhieuNHat
+    {
+        public Guid Id { get; set; }
+        public string Ten { get; set; }
+        public int SoHoaDon { get; set; }
+        public float TongTien { get; set; }
+        public int SoSanPhamBan { get; set; }
+
+    }
 }
